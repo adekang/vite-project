030cb49f651c2847e9da2bc5dff223f78ab739cd
adekang <adekang@163.com>
2023-02-08 17:28:12 +0800

    chore: build

--- config/config.ts
+++ config/config.ts
@@ -1,2 +1,2 @@
 export const VITE_APP_BASE = "/vite-project/";
-export const isProduction = process.env.NODE_ENV === "production";
+export const isDev = process.env.NODE_ENV === "development";
--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,13 @@ import windi from "vite-plugin-windicss";
 import cssOption from "./config/style";
 import createVitePlugins from "./config/plugins";
 import path from "path";
-import { isProduction, VITE_APP_BASE } from "./config/config";
+import { isDev, VITE_APP_BASE } from "./config/config";
 import build from "./config/build";
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
   console.log("config::", configEnv);
   const { command, mode } = configEnv;
-
-  const isDev = mode === "development";
-
   return {
     base: isDev ? "./" : VITE_APP_BASE,
     resolve: {
