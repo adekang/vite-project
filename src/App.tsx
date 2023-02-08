e5c3c723303c198bffa2873404673c1dfc41fdd2
adekang <adekang@163.com>
2023-02-08 15:55:25 +0800

    chore: alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./App.module.scss";
+import logoSrc from "@assets/imgs/vite.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
     <div className={styles.app}>
       <h1 border="2 rounded blue-200">12</h1>
       <div className="flex-c">center</div>
+      <img className="m-auto mb-4" src={logoSrc} alt="" />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import windi from "vite-plugin-windicss";
 import cssOption from "./config/style";
 import createVitePlugins from "./config/plugins";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
@@ -10,6 +11,13 @@ export default defineConfig((configEnv) => {
   const { command, mode } = configEnv;
 
   return {
+    resolve: {
+      // 别名配置
+      alias: {
+        "@": path.join(__dirname, "./src"),
+        "@assets": path.join(__dirname, "./src/assets")
+      }
+    },
     css: cssOption,
     plugins: [react(), windi(), createVitePlugins(command, mode)]
   };
