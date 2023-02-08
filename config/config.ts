f1817f6d771c304599eff7306b65811930860795
adekang <adekang@163.com>
2023-02-08 16:16:41 +0800

    chore: base 自定义部署域名

---
+++ config/config.ts
@@ -0,0 +1,2 @@
+export const VITE_APP_BASE = "xxxxxx";
+export const isProduction = process.env.NODE_ENV === "production";
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import windi from "vite-plugin-windicss";
 import cssOption from "./config/style";
 import createVitePlugins from "./config/plugins";
 import path from "path";
+import { isProduction, VITE_APP_BASE } from "./config/config";
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
   console.log("config::", configEnv);
   const { command, mode } = configEnv;
 
+  // const isProduction = mode === "production";
+
   return {
+    base: isProduction ? VITE_APP_BASE : "./",
     resolve: {
       // 别名配置
       alias: {
