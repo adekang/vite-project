c6d0c646b6aa9468291bb2546b16fd60dad951e4
adekang <adekang@163.com>
2023-02-09 18:45:08 +0800

    feat: icon封装

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import styles from "./App.module.scss";
 import logoSrc from "@/assets/imgs/vite.png";
 import SvgIcon from "@/components/SvgIcon";
-import { ReactComponent as ReactLogo } from "@/assets/icons/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const ok = () => {
+    console.log("ok");
+  };
 
   return (
     <div className={styles.app}>
       <h1 border="2 rounded blue-200">12</h1>
       <div className="flex-c">center</div>
       <img className="m-auto mb-4" src={logoSrc} alt="" />
-      <ReactLogo />
-      <SvgIcon iconName="react" color="red" />
+      <p>
+        我是react
+        <SvgIcon onClick={ok} iconName="react" className={styles.icon_font} />
+        你是
+      </p>
     </div>
   );
 }
--- src/components/SvgIcon/index.tsx
+++ src/components/SvgIcon/index.tsx
@@ -1,15 +1,33 @@
+import { CSSProperties } from "react";
+import styles from "./index.module.scss";
+import { CSSOptions } from "vite";
+
 export interface SvgIconProps {
   iconName: string;
-  color: string;
-  [key: string]: string;
+  className?: string;
+  style?: CSSProperties;
+  onClick?: () => void;
 }
 
-const SvgIcon = ({ iconName, color = "#333", ...props }: SvgIconProps) => {
-  const symbolId = `#${"icon"}-${iconName}`;
+const SvgIcon = ({ iconName, style, className, onClick }: SvgIconProps) => {
+  const symbolId = `#icon-${iconName}`;
+
+  const svgClass = () => {
+    if (className) {
+      return `${className} ` + styles.icon;
+    } else {
+      return styles.icon;
+    }
+  };
 
   return (
-    <svg {...props} aria-hidden="true">
-      <use href={symbolId} fill={color} />
+    <svg
+      className={svgClass()}
+      aria-hidden="true"
+      style={style}
+      onClick={onClick}
+    >
+      <use xlinkHref={symbolId} />
     </svg>
   );
 };
